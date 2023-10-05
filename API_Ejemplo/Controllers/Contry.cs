eec1adceb8036450d2690da85f88842a6b4b478f
MariaLimon <limon185mari@gmail.com>
2023-10-05 09:19:59 -0700

    Agregar archivos de proyecto.

---
+++ API_Ejemplo/Controllers/Contry.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+namespace API_Ejemplo.Controllers
+{
+    
+        public class ContryController : Controller
+        {
+            public async Task<IActionResult> GetCountryInfo()
+            {
+                string baseUrl = "https://restcountries.com/v3.1/name/";
+                string countryName = "Brasil";
+
+                using (HttpClient client = new HttpClient())
+                {
+                    try
+                    {
+                        HttpResponseMessage response = await client.GetAsync(baseUrl + countryName);
+
+                        if (response.IsSuccessStatusCode)
+                        {
+                            string data = await response.Content.ReadAsStringAsync();
+                            // Aquí puedes deserializar los datos JSON y acceder a la información del país.
+                            return Content(data, "application/json");
+                        }
+                        else
+                        {
+                            return Content($"Error: {response.StatusCode}");
+                        }
+                    }
+                    catch (Exception ex)
+                    {
+                        return Content($"Error: {ex.Message}");
+                    }
+                }
+            }
+        }
+}
+
+
